e49039211a2bf1fe5f160b63cdfb3e627aa6108d
Oğuzhan Kaya <oguzhan_kobe@hotmail.com>
2025-06-26 01:08:20 +0300

    Stack Done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,16 @@
 package org.example;
 
 import org.example.linear.LinkedList;
+import org.example.linear.Stack;
 
 public class Main {
     public static void main(String[] args) {
-
+        var stack = new Stack();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        var value = stack.pop();
+        System.out.println("Popped value:" + value);
+        stack.printStack();
     }
 }
---
+++ src/main/java/org/example/linear/Stack.java
@@ -0,0 +1,31 @@
+package org.example.linear;
+
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
+public class Stack {
+
+    private int[] stack;
+
+    public Stack() {
+        stack = new int[0];
+    }
+
+    public void push(int value) {
+        stack = Arrays.copyOf(stack, stack.length + 1);
+        stack[stack.length - 1] = value;
+    }
+    public int pop() {
+        if (stack.length == 0) {
+            throw new EmptyStackException();
+        }
+        var value = stack[stack.length - 1];
+        stack = Arrays.copyOf(stack, stack.length - 1);
+        return value;
+    }
+    public void printStack() {
+        for (int i = 0; i <= stack.length - 1; i++) {
+            System.out.print(stack[i] + " ");
+        }
+    }
+}
